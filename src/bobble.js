668582620c2b8a0e266ced6ded21519e64260bd5
Duncan Beevers <duncanbeevers@gmail.com>
2010-06-05 15:23:31 -0700

    Export explicit public API, make bobble-local variables available only to bobble-internal implementations of functions

--- src/bobble.js
+++ src/bobble.js
@@ -3,25 +3,39 @@
   Simulated time environment for JavaScript
 */
 function Bobble(src) {
-  var setTimeout = function() {};
-  var clearTimeout = function() {};
-  var setInterval = function() {};
-  var clearInterval = function() {};
-  var Date = function() {};
-  Date.prototype = {};
-  
-  var alert = function(s) { console.log("alert: %o", s); };
-  
-  var advanceToTime = (function() {
+  var BobblePublicAPI = (function() {
     var bobbleTime = 0;
-    return function(time) {
-      if (time < bobbleTime) {
-        throw("Can't go back in time");
-      } else {
-        bobbleTime = time;
+    var timeouts   = [];
+    var intervals  = [];
+    var tcPsh = function(cl, fn, ms) {
+      cl.push({
+        fn: fn,
+        ms: ms,
+        lastFired: 0
+      });
+      return cl.length;
+    };
+    
+    return {
+      setTimeout: function(fn, ms) { return tcPsh(timeouts, fn, ms); },
+      setInterval: function(fn, ms) { return tcPsh(intervals, fn, ms); },
+      advanceToTime: function(time) {
+        if (time < bobbleTime) {
+          throw("Can't go back in time");
+        } else {
+          bobbleTime = time;
+        }
       }
     };
   })();
+  
+  var setTimeout    = BobblePublicAPI.setTimeout;
+  var clearTimeout  = BobblePublicAPI.clearTimeout;
+  var setInterval   = BobblePublicAPI.setInterval;
+  var clearInterval = BobblePublicAPI.setInterval;
+  var Date          = BobblePublicAPI.Date;
+  var advanceToTime = BobblePublicAPI.advanceToTime;
+  var alert = function(s) { console.log("alert: %o", s); };
   
   eval(src);
 };
