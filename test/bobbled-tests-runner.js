d0d173024865d46ea22a33fe06767911f9129d5a
Duncan Beevers <duncan@dweebd.com>
2010-06-10 10:58:20 -0700

    Show messages from all assertions

--- test/bobbled-tests-runner.js
+++ test/bobbled-tests-runner.js
@@ -7,18 +7,16 @@ var TestPublicAPI = (function() {
   var summary_template = new Template('                                    \
   <div class="js-test-results-summary">                                    \
     <pre>#{successes_count}, #{failures_count}</pre>                       \
-  </div>                                                                   \
-  ');
+  </div>');
   
   var assertions_count;
   var failures = [];
   var successes = [];
   var body = document.getElementsByTagName('body')[0];
   
-  function pl(n, s) {
-    return 1 == n ? s : s + (s.match(/(s?)$/)[1] ? 'es' : 's');
-  };
+  function pl(n, s) { return 1 == n ? s : s + (s.match(/(s?)$/)[1] ? 'es' : 's'); };
   function pluralize(n, s) { return n + ' ' + pl(n, s); }
+  function cat(s1, s2) { return s1 ? s1 + "\n" + s2 : s2; }
   
   document.observe('dom:loaded', function() {
     $$('.js-test').each(function(t) {
@@ -39,7 +37,7 @@ var TestPublicAPI = (function() {
       
       var template_env = {
         function_body: t.innerHTML,
-        result_message: (result_message || '') + '\n' + pluralize(assertions_count, 'Assertion')
+        result_message: cat(result_message, pluralize(assertions_count, 'Assertion'))
       };
       if (failed) {
         template_env.test_result = 'failure';
@@ -60,12 +58,12 @@ var TestPublicAPI = (function() {
   return {
     assertEquals: function(expected, actual, message) {
       assertions_count++;
-      if (expected != actual) { throw(message || ("Expected " + expected + " to equal " + actual)); }
+      if (expected != actual) { throw(cat(message, ("Expected " + expected + " to equal " + actual))); }
     },
     assertThrows: function(fn, message) {
       var threw = false;
       try { fn(); } catch(_) { threw = true; }
-      assertEquals(true, threw, message || ("Expected " + fn + " to have thrown"));
+      assertEquals(true, threw, cat(message, ("Expected " + fn + " to have thrown")));
     }
   };
 })();
@@ -79,5 +77,4 @@ document.observe('click', function(e) {
       function(t){ t.setStyle({ display: 'block' }); }
     );
   }
-}
-);
+});
